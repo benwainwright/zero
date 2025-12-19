c335ed284060064a21adaffd2d36ef8249363a82
Ben Wainwright <bwainwright28@gmail.com>
2025-12-19 09:14:57 +0000

    add coverage

---
+++ packages/application-core/vitest.config.mts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/application-core',
+  plugins: [
+    tsconfigPaths({
+      projects: [
+        path.join(import.meta.dirname, 'tsconfig.lib.json'),
+        path.join(import.meta.dirname, 'tsconfig.spec.json'),
+      ],
+    }),
+  ],
+  test: {
+    name: '@zero/application-core',
+    watch: false,
+    globals: true,
+    environment: 'node',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      enabled: true,
+      include: ['./src/**/*.ts'],
+      exclude: ['./src/**/*.test.ts', '*./src/**/*.spec.ts'],
+      thresholds: {
+        autoUpdate: true,
+        functions: 6.89,
+        lines: 20.4,
+        statements: 20.19,
+        branches: 0,
+      },
+      reportsDirectory: './test-output/vitest/coverage',
+      provider: 'v8' as const,
+    },
+  },
+}));
--- packages/serialiser/vite.config.ts
+++ packages/serialiser/vite.config.ts
@@ -1,24 +1,21 @@
-/// <reference types='vitest' />
+import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(() => ({
   root: import.meta.dirname,
   cacheDir: '../../node_modules/.vite/packages/serialiser',
-  plugins: [],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [],
-  // },
-  test: {
-    name: '@zero/serialiser',
-    watch: false,
-    globals: true,
-    environment: 'node',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './test-output/vitest/coverage',
-      provider: 'v8' as const,
-    },
-  },
+  plugins: [
+    tsconfigPaths({
+      projects: [
+        path.join(import.meta.dirname, 'tsconfig.lib.json'),
+        path.join(import.meta.dirname, 'tsconfig.spec.json'),
+      ],
+    }),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(import.meta.dirname, 'tsconfig.lib.json'),
+    }),
+  ],
 }));
