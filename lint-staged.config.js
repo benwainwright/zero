44a41bb13a03136447ae749c0bd7cfb70bffab37
Ben Wainwright <bwainwright28@gmail.com>
2025-12-19 07:26:36 +0000

    fixed invalid tsconfig

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,14 @@
+import path from 'path';
+
 /**
  * @filename: lint-staged.config.js
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js,ts,tsx,jsx,json}': ['pnpm exec nx affected --targets test --files'],
+  '*.{js,ts,tsx,jsx,json}': [
+    (files) =>
+      `pnpm exec nx affected --targets test --files ${files
+        .map((file) => path.relative(process.cwd(), file))
+        .join(',')}`,
+  ],
 };
