ca55c3384f1a78b4cd6d1b848473b3f687fba6fb
Ben Wainwright <bwainwright28@gmail.com>
2026-01-12 18:59:50 +0000

    tweak logging

--- packages/bootstrap/src/lib/get-winston-logger.ts
+++ packages/bootstrap/src/lib/get-winston-logger.ts
@@ -2,24 +2,21 @@ import winston from 'winston';
 
 import type { ILogger } from '@types';
 
+const transports = () => {
+  return [
+    new winston.transports.Console({
+      format: winston.format.json(),
+    }),
+  ];
+};
+
 const getLogger = (): ILogger => {
   const logger = winston.createLogger({
     level: process.env['YNAB_PLUS_LOG_LEVEL'] ?? 'info',
     format: winston.format.json(),
     defaultMeta: { service: 'user-service' },
-    transports: [
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
-    ],
+    transports: transports(),
   });
-
-  if (process.env['NODE_ENV'] !== 'production') {
-    logger.add(
-      new winston.transports.Console({
-        format: winston.format.simple(),
-      })
-    );
-  }
 
   return logger;
 };
