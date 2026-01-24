cbd4700420a78a2fd19b7fa688a829a4c5a6138a
Ben Wainwright <bwainwright28@gmail.com>
2026-01-24 17:47:22 +0000

    get env vars from service

--- apps/web-e2e/src/tests/login.spec.ts
+++ apps/web-e2e/src/tests/login.spec.ts
@@ -5,8 +5,12 @@ test('login works', async ({ page }) => {
   const loginPage = new LoginPageObject(page);
 
   await loginPage.goto();
-  await loginPage.username.fill('admin');
-  await loginPage.password.fill('password');
+  await loginPage.username.fill(
+    process.env['ZERO_CONFIG_AUTH_ADMINEMAIL'] ?? 'admin'
+  );
+  await loginPage.password.fill(
+    process.env['ZERO_CONFIG_AUTH_ADMINPASSWORD'] ?? 'password'
+  );
   await loginPage.loginButton.click();
 
   await expect(page.getByText('Home')).toBeVisible();
