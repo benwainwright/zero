2567e620d4ce4e12fc463c7e9583903fc5c86516
Ben Wainwright <bwainwright28@gmail.com>
2026-01-24 17:53:22 +0000

    fix test

--- apps/web-e2e/src/tests/login.spec.ts
+++ apps/web-e2e/src/tests/login.spec.ts
@@ -1,17 +1,18 @@
 import { test, expect } from '@playwright/test';
 import { LoginPageObject } from '@poms';
 
-test('login works', async ({ page }) => {
+test('When I enter the admin username and password and press login I am taken to the home page', async ({
+  page,
+}) => {
   const loginPage = new LoginPageObject(page);
 
   await loginPage.goto();
-  await loginPage.username.fill(
-    process.env['ZERO_CONFIG_AUTH_ADMINEMAIL'] ?? 'admin'
-  );
+  await loginPage.username.fill('admin');
+
   await loginPage.password.fill(
     process.env['ZERO_CONFIG_AUTH_ADMINPASSWORD'] ?? 'password'
   );
   await loginPage.loginButton.click();
 
-  await expect(page.getByText('Home')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible();
 });
