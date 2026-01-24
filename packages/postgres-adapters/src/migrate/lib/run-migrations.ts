0938ac6a4d741251a637c8e097499577abad6aba
Ben Wainwright <bwainwright28@gmail.com>
2026-01-24 13:17:58 +0000

    remove wait

--- packages/postgres-adapters/src/migrate/lib/run-migrations.ts
+++ packages/postgres-adapters/src/migrate/lib/run-migrations.ts
@@ -2,13 +2,7 @@ import { type ConnectConfig } from './connect.ts';
 import { logResults } from './log-results.ts';
 import { migrator as getMigrator } from './migrator.ts';
 
-import waitPort from 'wait-port';
-
 export const runMigrations = async (config: ConnectConfig) => {
-  await waitPort({
-    host: config.host ?? 'localhost',
-    port: config.port ?? 5432,
-  });
   const migrator = await getMigrator(config);
 
   const result = await migrator.migrateToLatest();
